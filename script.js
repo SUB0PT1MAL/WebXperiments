7aa1394cadb22b19dbc0246ecb5463f36c551427
SUB0PT1MAL <2112blar@gmail.com>
2023-09-23 16:50:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,39 @@
-// Tree config 
-const config = {
-  length: 10,
-  lifeStart: 32,
-  branchMultiplier: 5,
-  leaves: ["~", "*", "#", "@"]  
+function Bonsai() {
+  this.pot = document.createElement("div");
+  this.pot.className = "bonsai-pot";
+
+  this.trunk = document.createElement("div");
+  this.trunk.className = "bonsai-trunk";
+
+  this.leaves = document.createElement("div");
+  this.leaves.className = "bonsai-leaves";
+
+  this.document = document;
+  this.body = document.body;
+
+  this.pot.appendChild(this.trunk);
+  this.trunk.appendChild(this.leaves);
+
+  this.body.appendChild(this.pot);
+
+  this.grow();
+}
+
+Bonsai.prototype.grow = function() {
+  this.trunk.style.height = this.trunk.style.height + 10 + "px";
+  this.leaves.style.width = this.leaves.style.width + 10 + "px";
+
+  if (this.trunk.style.height >= 500) {
+    clearInterval(this.interval);
+  }
 };
 
-// Main tree object
-let tree = {};
+Bonsai.prototype.start = function() {
+  this.interval = setInterval(this.grow.bind(this), 100);
+};
 
-// Initialization
-function init() {
+Bonsai.prototype.stop = function() {
+  clearInterval(this.interval);
+};
 
-  // Create trunk
-  tree.trunk = {
-    x: 0, 
-    y: 0,
-    branches: [],
-    life: config.lifeStart 
-  };
-
-  draw();
-}
-
-// Main draw loop
-function draw() {
-
-  // Determine display size
-  let minX = 0;
-  let maxX = 0;
-  let minY = 0;
-  let maxY = tree.trunk.y;
-
-  // Initialize display 
-  let display = [];
-  for (let y = minY; y < maxY; y++) {
-    display[y] = [];
-  }
-
-  // Add new branches
-  if (Math.random() < 0.1 && tree.trunk.life > config.branchMultiplier) {
-    let branch = {
-      x: tree.trunk.x,
-      y: tree.trunk.y + 1,
-      life: tree.trunk.life / 2
-    };
-    tree.trunk.branches.push(branch);
-  }
-
-  // Update all segments
-  updateSegment(tree.trunk);
-  for (let branch of tree.trunk.branches) {
-    updateSegment(branch);
-  }
-
-  // Helper to update segment position and life
-  function updateSegment(segment) {
-    segment.x += Math.random() > 0.5 ? 1 : -1;
-    segment.y++; 
-    segment.life--;
-  }
-
-  // Build display array 
-  for (let seg of [tree.trunk, ...tree.trunk.branches]) {
-    if (!display[seg.y]) {
-      display[seg.y] = [];
-    }
-    display[seg.y][seg.x] = config.leaves[Math.floor(Math.random() * config.length)];
-  }
-
-  // Render tree
-  let treeHTML = "";
-  for (let y = 0; y < display.length; y++) {
-    for (let x = 0; x < display[y].length; x++) {
-      if (display[y][x]) {
-        treeHTML += display[y][x];
-      } else {
-        treeHTML += " ";  
-      }
-    }
-    treeHTML += "<br>";
-  }
-
-  document.getElementById("tree").innerHTML = treeHTML;
-
-  requestAnimationFrame(draw);
-}
-
-init();
+window.Bonsai = Bonsai;
